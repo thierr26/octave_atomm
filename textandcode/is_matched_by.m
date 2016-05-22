eeec5cd9e954e9b025d1de354b673eaf347a6bdc
Thierry Rascle <thierr26@free.fr>
2016-05-22 17:24:40 +0200

    Accept cellstr arguments for is_matched_by

--- textandcode/is_matched_by.m
+++ textandcode/is_matched_by.m
@@ -6,13 +6,24 @@
 ##
 ## Return true if the string @var{str} is matched by the regular expression
 ## @var{expr}.
+##
+## @var{str} can be a cell array of strings. In this case, @code{is_matched_by}
+## returns a logical array (same shape as @var{str}).
+##
+## @seealso{is_string, iscellstr}
 ## @end deftypefn
 
 ## Author: Thierry Rascle <thierr26@free.fr>
 
 function ret = is_matched_by(str, expr)
 
-    validated_mandatory_args({@is_string, @is_non_empty_string}, str, expr);
-    ret = ~isempty(regexp(str, expr, 'once'));
+    validated_mandatory_args(...
+        {@(x) is_string(x) || iscellstr(x), @is_non_empty_string}, str, expr);
+
+    if iscell(str)
+        ret = cellfun(@(x) ~isempty(x), regexp(str, expr, 'once'));
+    else
+        ret = ~isempty(regexp(str, expr, 'once'));
+    endif
 
 endfunction
