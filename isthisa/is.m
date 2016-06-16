791d1e8a51f4ffcb0244a69293942793e225795b
Thierry Rascle <thierr26@free.fr>
2016-06-16 22:09:16 +0200

    New non anonymous function that is always true
    * Checkmtree did not work on Matlab when anonymous function '@() true'
      was used instead.

---
+++ isthisa/is.m
@@ -0,0 +1,16 @@
+## Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
+## MIT license. Please refer to the LICENSE file.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} is ()
+##
+## Return true.
+## @end deftypefn
+
+## Author: Thierry Rascle <thierr26@free.fr>
+
+function ret = is
+
+    ret = true;
+
+end
