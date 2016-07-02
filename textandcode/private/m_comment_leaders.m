af9f09cc10eefa3607b9a9232463c34c59bab0f7
Thierry Rascle <thierr26@free.fr>
2016-07-02 21:31:09 +0200

    Remove useless textandcode's private directory

--- textandcode/private/m_comment_leaders.m
+++
@@ -1,9 +0,0 @@
-## Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
-## MIT license. Please refer to the LICENSE file.
-## Author: Thierry Rascle <thierr26@free.fr>
-
-function str = m_comment_leaders
-
-    str = '%#';
-
-endfunction
