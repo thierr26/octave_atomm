03ac6fb14ecafe8927f5e87455630b1530936fed
Thierry Rascle <thierr26@free.fr>
2016-07-02 22:24:29 +0200

    Add new text_file_lines function

---
+++ textandcode/text_file_lines.m
@@ -0,0 +1,43 @@
+## Copyright (C) 2016 Thierry Rascle <thierr26@free.fr>
+## MIT license. Please refer to the LICENSE file.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} text_file_lines (@var{filename})
+##
+## Lines of a text file.
+##
+## @code{text_file_lines} returns a cell array of strings.  Each cell contains
+## a line of the text file @var{filename}.
+## @end deftypefn
+
+## Author: Thierry Rascle <thierr26@free.fr>
+
+function c = text_file_lines(filename)
+
+    validated_mandatory_args({@is_non_empty_string}, filename);
+
+    # Open the file.
+    f = fopen(filename, 'r');
+
+    # Move to the end of the file.
+    fseek(f, 0, 'eof');
+
+    if ftell(f) == 0
+        # The file has zero bytes.
+
+        c = {};
+    else
+        # The file has at least one byte.
+
+        # Come back to the beginning of the file.
+        fseek(f, 0, 'bof');
+
+        # Read the lines of the file.
+        c = textscan(f, '%s', 'Delimiter', '\n', 'whitespace', '');
+        c = c{1};
+    endif
+
+    # Close the file.
+    fclose(f);
+
+endfunction
