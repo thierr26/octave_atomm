0a3b1473e289cf665e56b5b5e36ef41bf56976c3
Thierry Rascle <thierr26@free.fr>
2020-05-01 12:44:58 +0200

    Fix prod_exact_for_10_pow (Octave 5.2.0 Windows)

--- math/prod_exact_for_10_pow.m
+++ math/prod_exact_for_10_pow.m
@@ -12,18 +12,12 @@
 ## @code{@var{ret} = prod_exact_for_10_pow (@var{a}, @var{b})} or
 ## @code{@var{ret} = prod_exact_for_10_pow (@var{a}, @var{b}, @var{tolerance})}
 ## returns in @var{ret} the product of @var{a} and @var{b}.  The product is
-## calculated with one of the following statements:
-##
-## @itemize @bullet
-## @item
-## @code{@var{ret} = 10^(round(log10(@var{a})) + round(log10(@var{b}))} if both
+## calculated with @code{@var{ret} = @var{a} * @var{b}} except if both
 ## @code{log10(@var{a})} and @code{log10(@var{b})} are integers, as determined
 ## by @code{is_integer_scalar} called with a tolerance argument set to
-## @code{1e-6} or @var{tolerance} if argument @var{tolerance} is provided.
-##
-## @item
-## @code{@var{ret} = @var{a} * @var{b}} otherwise.
-## @end itemize
+## @code{1e-6} or @var{tolerance} if argument @var{tolerance} is provided. In
+## this case, the product is performed by evaluating a scientific notation
+## (e.g.@ "1e-12").
 ##
 ## The point of @code{prod_exact_for_10_pow} is to make product of powers of
 ## ten exact.  Multiplying powers of ten with operator @code{"*"} may give
@@ -52,7 +46,7 @@ function ret = prod_exact_for_10_pow(a, b, varargin)
     if is_integer_scalar(la, tolerance)
         lb = log10(b);
         if is_integer_scalar(lb, tolerance)
-            ret = 10^(round(la) + round(lb));
+            ret = eval(['1e' num2str(round(la) + round(lb))]);
             done = true;
         endif
     endif
